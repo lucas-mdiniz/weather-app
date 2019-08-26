67602f8d4af78105bc6ebf176b7ec1d0f4124d70
lucas-mdiniz <42212342+lucas-mdiniz@users.noreply.github.com>
2019-08-26 12:48:56 -0300

    Update weather.js

--- weather.js
+++ weather.js
@@ -3,8 +3,8 @@ class GetWeather{
     this.apiKey = 'd176520305ac82fb8b49ba931687a60a';
   }
 
-  async getData(city){
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}`);
+  async getData(lat, lon){
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}`);
     
     const responseData = await response.json();
 
